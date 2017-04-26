de68a6c23cd96d83b13c72ed1b10566b93dca5f6
logicmyth <staley77@protonmail.com>
2017-04-26 14:04:22 -0400

    add handlers to index.js

--- jcsJsFile/assets/scripts/auth/events.js
+++ jcsJsFile/assets/scripts/auth/events.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 //
-const getFormFields = require('../../../lib/get-form-fields');
+const getFormFields = require('../../../lib/get-form-fields')
 
 const voidText = function () {
-  console.log('voidText');
-  const text = getFormFields(event.target);
-  console.log(text);
-};
+  console.log('voidText')
+  const text = getFormFields(event.target)
+  console.log(text)
+}
 
 const addHandlers = function () {
-  $('.twitterSubmit').on('click', voidText);
-};
+  $('.twitterSubmit').on('click', voidText)
+}
 
 module.exports = {
   addHandlers
-};
+}
